a953853bf7a2b26df56fb3261f27138469052662
Damien Stamates <thedoubledimensions@gmail.com>
2019-09-03 01:57:40 -0500

    Added simple pixel example

---
+++ examples/pixel/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"image"
+	_ "image/png"
+	"os"
+	"time"
+
+	"github.com/damienfamed75/aseprite"
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
+
+const (
+	screenWidth  = 320
+	screenHeight = 240
+)
+
+func main() {
+	pixelgl.Run(run)
+}
+
+func loadPicture(path string) (pixel.Picture, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	return pixel.PictureDataFromImage(img), nil
+}
+
+func run() {
+	// Create a new pixel window.
+	cfg := pixelgl.WindowConfig{
+		Title:     "Demo",
+		Bounds:    pixel.R(0, 0, screenWidth, screenHeight),
+		Resizable: true,
+	}
+	win, _ := pixelgl.NewWindow(cfg)
+
+	// Open the player spritesheet's aseprite JSON file.
+	ase, _ := aseprite.Open("player.json")
+	ase.Play("left")
+
+	// Open the player image
+	pic, err := loadPicture("player.png")
+	if err != nil {
+		panic(err)
+	}
+
+	// Creates a pixel Matrix Drawing object.
+	imd := imdraw.New(pic)
+	sprite := pixel.NewSprite(pic, pic.Bounds())
+	last := time.Now()
+	for !win.Closed() {
+		// Calculate delta time. I'm sure there's a better option on how to
+		// create this, because everytime I run this the animation stutters.
+		dt := float32(time.Since(last).Milliseconds()) / 100
+		last = time.Now()
+
+		ase.Update(dt)
+		bounds := ase.FrameBoundaries().Rectangle()
+
+		sprite.Set(pic, pixel.R(
+			float64(bounds.Min.X), float64(bounds.Min.Y),
+			float64(bounds.Max.X), float64(bounds.Max.Y)),
+		)
+		sprite.Draw(
+			win, pixel.IM.Scaled(pixel.ZV, 4.0).Moved(
+				win.Bounds().Center(),
+			),
+		)
+		imd.Draw(win)
+
+		win.Update()
+		win.Clear(colornames.Skyblue)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,8 +4,15 @@ go 1.12
 
 require (
 	github.com/damienfamed75/goaseprite v0.0.0-20180826090229-a959a0424110 // indirect
+	github.com/faiface/glhf v0.0.0-20181018222622-82a6317ac380 // indirect
+	github.com/faiface/mainthread v0.0.0-20171120011319-8b78f0a41ae3 // indirect
+	github.com/faiface/pixel v0.8.0
+	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7 // indirect
+	github.com/go-gl/mathgl v0.0.0-20190713194549-592312d8590a // indirect
 	github.com/hajimehoshi/ebiten v1.9.3
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/solarlune/goaseprite v0.0.0-20190529165257-8ddb57cec83c
 	github.com/tidwall/gjson v1.3.2 // indirect
 	gitlab.com/c0b/go-ordered-json v0.0.0-20171130231205-49bbdab258c2
+	golang.org/x/image v0.0.0-20190321063152-3fc05d484e9f
 )
